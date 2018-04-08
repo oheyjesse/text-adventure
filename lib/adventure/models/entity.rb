914980fc396e91cbb5013a2e609d1720cfdf5708
oheydrew <drew@oheydrew.me>
2018-04-08 18:15:47 +1000

    Add player, enemy subclasses

--- lib/adventure/models/entity.rb
+++ lib/adventure/models/entity.rb
@@ -7,19 +7,12 @@
 # NYI: player class inherits from Entity class
 # NYI: monster class inherits from entity class
 
-#   entity.move(direction)
-#   entity.attack(entity, weapon)
-#   entity.droploot(location)
-#   entity.pickup(loot)
-#   entity.use(item)
-
 class Entity
 
   attr_reader :name, :type, :health, :weapon, :items
   attr_accessor :coords
 
-  def initialize(coords, type, playername, health, weapon, items)
-    @type = type
+  def initialize(coords, playername, health, weapon, items)
     @name = playername
     @health = health
     @weapon = weapon
@@ -37,10 +30,8 @@ class Entity
 
     if validate_move(world, new_coords)
       @coords = new_coords
-
       world[old_coords[0]][old_coords[1]].player_present = false
       world[new_coords[0]][new_coords[1]].player_present = true
-
       engine_msg(self, "moved from #{old_coords} (#{world[old_coords[0]][old_coords[1]].name}) to #{@coords} (#{world[@coords[0]][@coords[1]].name})")
     else
       puts "The way is blocked. You can't go that way.".colorize(:light_red)
@@ -51,30 +42,24 @@ class Entity
   def attack(enemy)
     # attacks with currently assigned weapon
   end
-
-  def defend(weapon)
-    # defends against attacks from enemies, 
-    # and lowers health accordingly (entity.health_lower)
-  end
-
+  
   def pick_up(item)
     # removes item from where it resides
     # places item in entity's inventory
   end
-
+  
   def drop(item)
     # removes item from entity's inventory
     # places item in entity.location's inventory
   end
-
+  
   def use(item)
     # determines item type
     # passes item to correct method (equip, use_healthpack etc)
   end
-
   
   #(direction, location or entity)
-  def look(thing) # object
+  def look_at(thing) # object
     look = ''
     case thing
     when Location
@@ -85,16 +70,21 @@ class Entity
       "you can't see that thing."
     end
   end
-
-  def location(world)
+  
+  def defend_against(weapon)
+    # defends against attacks from enemies,
+    # and lowers health accordingly (entity.health_lower)
+  end
+  
+  def location_in(world)
     world[@coords[0]][@coords[1]]
   end
-
+  
   # ---------------------------------------------- Private Methods
   private
-
+  
   def parse_direction(direction)
-    # takes direction as a string, compares it against COMPASS and 
+    # takes direction as a string, compares it against COMPASS and
     # returns corresponding direction modifier array
     COMPASS[direction.to_sym]
   end
@@ -104,17 +94,17 @@ class Entity
     new_coords = coords.zip(dir_modifier).map { |arr| arr.sum }
     return new_coords
   end
-  
+
   # not yet fully implemented
   def validate_move(world, new_coords)
     true if new_coords[0].between?(0, world.size-1) && new_coords[1].between?(0, world.size-1)
   end
-  
+
   def equip_weapon(weapon)
     # places weapon into inventory
     # equips weapon to current slot
   end
-  
+
   def use_health(item)
     # increases health (entity.health_raise)
     # destroys item
